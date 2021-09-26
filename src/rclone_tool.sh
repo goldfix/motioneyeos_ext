0335bcf75e2de8d6f61ca6109588ffecd8039650
pietro partescano <p.pietro@gmail.com>
2021-09-26 15:31:43 +0200

    added option to copy or move files

--- src/rclone_tool.sh
+++ src/rclone_tool.sh
@@ -11,6 +11,7 @@ PROG_DEST_FILE="${PROG_DEST_FOLDER}/rclone"
 PROG_LOCAL_CONF="${PROG_BIN_FOLDER}/rclone_local.config"
 PROG_CONF="${PROG_BIN_FOLDER}/rclone.config"
 RCLONE_DEST=""
+RCLONE_OP=""
 
 cat << EOF > ${PROG_PY_UNZIP}
 import zipfile
@@ -20,15 +21,32 @@ with zipfile.ZipFile('/tmp/rclone.zip', "r") as z:
 EOF
 
 run() {
+    if [ ! -f "${PROG_LOCAL_CONF}" ] || [ ! -f "${PROG_CONF}" ];
+    then
+        echo "Missing configuration files. Plese run command: 'rclone_tool config'."
+        exit 1
+    fi
+    if [ "${RCLONE_OP}" = "" ]
+    then
+        echo "Missing Rclone type action ('copy' or 'move')."
+        exit 1
+    fi
     if [ "${RCLONE_DEST}" = "" ]
     then
         echo "Missing Rclone destination name."
         exit 1
     fi
 
-   echo Moving files to remote folder...
-   source ${PROG_LOCAL_CONF}
-   ${PROG_DEST_FILE} move ${CAM_FOLDER} ${RCLONE_DEST}:${REMOTE_FOLDER} --skip-links --log-file /var/log/rclone.log --log-level INFO --config ${PROG_CONF}
+    echo Moving files to remote folder...
+    source ${PROG_LOCAL_CONF}
+    if [ "${RCLONE_OP}" = "move" ]
+    then
+        ${PROG_DEST_FILE} ${RCLONE_OP} ${CAM_FOLDER} ${RCLONE_DEST}:${REMOTE_FOLDER} --delete-empty-src-dirs --skip-links --log-file /var/log/rclone.log --log-level INFO --config ${PROG_CONF}
+    fi
+    if [ "${RCLONE_OP}" = "copy" ]
+    then
+        ${PROG_DEST_FILE} ${RCLONE_OP} ${CAM_FOLDER} ${RCLONE_DEST}:${REMOTE_FOLDER} --skip-links --log-file /var/log/rclone.log --log-level INFO --config ${PROG_CONF}
+    fi
 }
 
 install() {
@@ -72,11 +90,17 @@ config() {
 
     echo CAM_FOLDER=${CAM_FOLDER} > ${PROG_LOCAL_CONF}
     echo REMOTE_FOLDER=${REMOTE_FOLDER} >> ${PROG_LOCAL_CONF}
+
+    chmod ug+r ${PROG_LOCAL_CONF}
+    chmod ug+r ${PROG_CONF}
+    chmod ugo+rx ${PROG_BIN_FOLDER}/rclone_tool.sh
+    chmod ugo+rx ${PROG_BIN_FOLDER}/rclone
 }
 
 case "$1" in
     run)
-        RCLONE_DEST=${2}
+        RCLONE_OP=${2}
+        RCLONE_DEST=${3}
         run
         ;;
 
@@ -89,7 +113,7 @@ case "$1" in
         config
         ;;
     *)
-        echo "Usage: $0 {install|config|run <RCLONE_NAME_DEST>}"
+        echo "Usage: $0 {install|config|run <copy|move> <RCLONE_NAME_DEST>}"
         exit 1
         ;;
 esac
