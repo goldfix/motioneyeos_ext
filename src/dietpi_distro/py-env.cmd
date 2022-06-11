470b26a4c019cf30bde938b8f38a3c591e32ac57
goldfix <p.pietro@gmail.com>
2022-06-11 19:31:49 +0200

    typo

--- src/dietpi_distro/py-env.cmd
+++ src/dietpi_distro/py-env.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-c:\Python3\python.exe -m venv --copies --clear ./py-env 
+c:\Python3\python.exe -m venv --copies --clear ./py-env
 call .\py-env\Scripts\activate
 pip install -r requirements.txt
